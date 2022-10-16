a11bffdd250fdb893c9ada8799d15392ed7583d8
Shubham Tejani <64226852+shubham404-error@users.noreply.github.com>
2022-10-16 23:03:49 +0530

    Update and rename streamlit.py to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ Created on Sun Oct 16 22:20:14 2022
 import pickle as pkl
 import streamlit as st
 
-model=pkl.load(open(r"C:\Users\ASUS\Downloads\model.sav",'rb'))
+model=pkl.load(open(r"model.sav",'rb'))
 
 def diab_pred(input_data):
     prediction=model.predict(input_data)
